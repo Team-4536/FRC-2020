335fe22027b09a0e0909774f5da35437c737d512
crn-caro <crn@7fff.com>
2020-01-11 11:44:38 -0600

    Snap to angle written
    TODO: tuning, constants

---
+++ src/main/java/frc4536/robot/commands/SnapToAngle.java
@@ -0,0 +1,62 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc4536.robot.commands;
+
+import frc4536.robot.subsystems.DriveTrain;
+import frc4536.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class SnapToAngle extends CommandBase {
+  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+  private final DriveTrain m_driveTrain;
+  private final double m_goalAngle;
+  //TODO: Implement constants
+  private final TrapezoidProfile.Constraints m_constraints = new TrapezoidProfile.Constraints(500, 500);
+  private final ProfiledPIDController m_controller = new ProfiledPIDController(1, 0, 0.3, m_constraints);
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public SnapToAngle(final DriveTrain driveTrain, double goalAngle) {
+    m_driveTrain = driveTrain;
+    m_goalAngle = goalAngle;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveTrain);
+  }
+  
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_controller.reset(m_driveTrain.getHeading());
+    m_controller.setGoal(m_goalAngle);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_driveTrain.arcadeDrive(0, m_controller.calculate(m_driveTrain.getHeading()));
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(final boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
