95933d2a05db3d54ad3d53e47ce57d6e3680f4fa
Tiger2222 <35354243+Tiger2222@users.noreply.github.com>
2020-01-14 17:06:33 -0600

    Update TrajectoryFactory.java

--- src/main/java/frc4536/robot/TrajectoryFactory.java
+++ src/main/java/frc4536/robot/TrajectoryFactory.java
@@ -2,6 +2,7 @@ package frc4536.robot;
 
 import java.util.List;
 
+import edu.wpi.first.wpilibj.controller.RamseteController;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
@@ -10,35 +11,9 @@ import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 
 public class TrajectoryFactory {
-    double intX;
-    double intY;
-    double endX;
-    double endY;
-    double intTany;
-    double intTanx;
-    double endTany;
-    double endTanx;
-    double maxSpeed;
-    double maxAcc;
+    RamseteController TrenchyPathManager;
     public TrajectoryFactory(double intX, double intY, double endX, double endY, double intTany,double intTanx,double endTany,double endTanx){ 
-        this.intX = intX;
-        this.intY = intY;
-        this.endX = endX;
-        this.endY = endY;
-        this.intTanx = intTanx;
-        this.intTany = intTany;
-        this.endTanx = endTanx;
-        this.endTany = endTany;
+        TrenchyPathManager = new RamseteController(2.0,0.7);
     }
-    TrajectoryConfig config = new TrajectoryConfig(maxSpeed, maxAcc);
-            //.setKinematics(DriveConstants.kDriveKinematics)
-            //.addConstraint(autoVoltageConstraint);
-
-    Trajectory frontTrajectory = TrajectoryGenerator.generateTrajectory(
-    new Pose2d(intX, intY, new Rotation2d(Math.atan2(intTany, intTanx))),
-    List.of(
-        new Translation2d(4.280339775896371,-4.349554667197974),
-        new Translation2d(6.500980548613312,-5.9819462285991705)),
-    new Pose2d(endX,endY, new Rotation2d(Math.atan2(endTany, endTany))),
-    config);
+    
 }
