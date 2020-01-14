01476b43a59dd7e06c33b7876f758d5eef25bae1
Sophieingersoll <singersoll001@stpaul.k12.mn.us>
2020-01-14 17:47:11 -0600

    implemented NEO smart motor

--- src/main/java/frc4536/lib/NEOSmartMotor.java
+++ src/main/java/frc4536/lib/NEOSmartMotor.java
@@ -1,83 +1,90 @@
 package frc4536.lib;
 
+import java.util.ArrayList;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class NEOSmartMotor implements ISmartMotor {
-private CANSparkMax motor;
+private ArrayList<CANSparkMax> motors = new ArrayList<>();
+private CANEncoder encoder;
+private CANPIDController pid;
+private double setpoint;
 
-public NEOSmartMotor(int deviceID){
-    motor = new CANSparkMax(deviceID, MotorType.kBrushless);
+public NEOSmartMotor(double[] constants, int...deviceID){
+    for(int id : deviceID){
+        motors.add(new CANSparkMax(id, MotorType.kBrushless));
+    }
+    encoder = motors.get(0).getEncoder();
+    pid = motors.get(0).getPIDController();
+    if(constants.length == 1) pid.setP(constants[0]);
+    if(constants.length == 2) pid.setI(constants[1]);
+    if(constants.length == 3) pid.setD(constants[2]);
+    if(constants.length == 4) pid.setIZone(constants[3]);
+    if(constants.length == 5) pid.setFF(constants[4]);
 }
     @Override
     public void set(double speed) {
-        
-    }
+        motors.forEach(m -> m.set(speed));
+    }   
 
     @Override
     public double get() {
-        // TODO Auto-generated method stub
-        return 0;
+        return motors.get(0).get();
     }
 
     @Override
     public void setInverted(boolean isInverted) {
-        // TODO Auto-generated method stub
-
+        motors.forEach(m -> m.setInverted(isInverted));
     }
 
     @Override
     public boolean getInverted() {
-        // TODO Auto-generated method stub
-        return false;
+        return motors.get(0).getInverted();
     }
 
     @Override
     public void disable() {
-        // TODO Auto-generated method stub
-
+        motors.forEach(m -> m.disable());
     }
 
     @Override
     public void stopMotor() {
-        // TODO Auto-generated method stub
-
+        motors.forEach(m -> m.stopMotor());
     }
 
     @Override
     public void pidWrite(double output) {
-        // TODO Auto-generated method stub
-
+        motors.forEach(m -> m.pidWrite(output));
     }
 
     @Override
     public void setVolt(double i) {
-        // TODO Auto-generated method stub
-
+        motors.forEach(m -> m.setVoltage(i));
     }
 
     @Override
     public void setSpeed(double i) {
-        // TODO Auto-generated method stub
-
+        setpoint = i;
+        pid.setReference(setpoint, ControlType.kVelocity);
     }
 
     @Override
     public double getSpeed() {
-        // TODO Auto-generated method stub
-        return 0;
+        return encoder.getVelocity();
     }
 
     @Override
     public double getDistance() {
-        // TODO Auto-generated method stub
-        return 0;
+        return encoder.getPosition();
     }
 
     @Override
     public double getSetpoint() {
-        // TODO Auto-generated method stub
-        return 0;
+        return setpoint;
     }
 
 
