79de834ec87ec1e95333d6335f2d070d50fcf781
Kevin Tran <kinzerfest@gmail.com>
2020-01-16 16:02:04 -0600

    Added encoders

--- src/main/java/frc4536/robot/Robot.java
+++ src/main/java/frc4536/robot/Robot.java
@@ -1,8 +1,10 @@
 package frc4536.robot;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
@@ -14,7 +16,15 @@ public class Robot extends TimedRobot {
     new SpeedControllerGroup(new Spark(2), new Spark(3))
   );
 
+  Encoder left = new Encoder(0,1), right = new Encoder(2,3);
+
   Joystick j = new Joystick(0);
+
+  @Override
+  public void robotInit(){
+    SmartDashboard.putData(left);
+    SmartDashboard.putData(right);
+  }
 
   @Override
   public void teleopPeriodic() {
