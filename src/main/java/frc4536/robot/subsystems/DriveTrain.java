4e43ada97228c317d7967f99334d934694ff4faa
rogereasthouse84 <57774854+rogereasthouse84@users.noreply.github.com>
2020-01-18 10:44:10 -0600

    Moved Features From og logging

--- src/main/java/frc4536/robot/subsystems/DriveTrain.java
+++ src/main/java/frc4536/robot/subsystems/DriveTrain.java
@@ -3,6 +3,9 @@ package frc4536.robot.subsystems;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc4536.lib.ISmartMotor;
 
@@ -17,6 +20,14 @@ public class DriveTrain extends SubsystemBase {
         m_rightMotor = rightMotor;
         m_navx = navx;
         m_drive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+        ComplexWidget tankDriveTab = Shuffleboard.getTab("Tank Drive Data")
+        .add("Tank Drive", m_drive);
+    }
+    ShuffleboardTab motorBasicTab = Shuffleboard.getTab("Motor Data");
+
+    public void periodic() {
+        motorBasicTab.add("Distance Travelled", getDistance());
+        motorBasicTab.add("Heading", getHeading());
     }
 
     public void curvatureDrive(double speed, double rotation, boolean quickTurn) {
