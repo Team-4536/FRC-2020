b00204a7f61cf0323767ab5f93b058641bc383f2
iCalculated <fufa0001@gmail.com>
2020-01-23 19:42:02 -0600

    ramsete with proper parameters

--- src/main/java/frc4536/robot/RobotContainer.java
+++ src/main/java/frc4536/robot/RobotContainer.java
@@ -7,13 +7,25 @@
 
 package frc4536.robot;
 
+import java.util.List;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc4536.robot.commands.ExampleCommand;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj.trajectory.constraint.TrajectoryConstraint;
+import frc4536.robot.commands.RamseteAutonomousCommand;
 import frc4536.robot.commands.TankDriveCommand;
 import frc4536.robot.hardware.RobotFrame;
 import frc4536.robot.hardware.TestRobot;
-import frc4536.robot.hardware.Trenchy;
+import frc4536.robot.hardware.IRobotConstants.AutoConstants;
+import frc4536.robot.hardware.IRobotConstants.DriveConstants;
 import frc4536.robot.subsystems.DriveTrain;
 import frc4536.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -33,7 +45,38 @@ public class RobotContainer {
                                                          m_robotHardware.getDrivetrainRightMotor(), 
                                                          m_robotHardware.getDrivetrainNavX());
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+
+  // Create a voltage constraint to ensure we don't accelerate too fast
+  TrajectoryConstraint autoVoltageConstraint =
+      new DifferentialDriveVoltageConstraint(
+          new SimpleMotorFeedforward(DriveConstants.ksVolts,
+                                      DriveConstants.kvVoltSecondsPerMeter,
+                                      DriveConstants.kaVoltSecondsSquaredPerMeter),
+                                      DriveConstants.kDriveKinematics,
+                                      10);
+  TrajectoryConfig m_config =
+      new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond,
+                           AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+          // Add kinematics to ensure max speed is actually obeyed
+          .setKinematics(DriveConstants.kDriveKinematics)
+          // Apply the voltage constraint
+          .addConstraint(autoVoltageConstraint);
+
+  private final Trajectory m_trajectory = TrajectoryGenerator.generateTrajectory(
+         // Start at the origin facing the +X direction
+         new Pose2d(0, 0, new Rotation2d(0)),
+         // Pass through these two interior waypoints, making an 's' curve path
+         List.of(
+             new Translation2d(1, 1),
+             new Translation2d(2, -1)
+         ),
+         // End 3 meters straight ahead of where we started, facing forward
+         new Pose2d(3, 0, new Rotation2d(0)),
+         // Pass config
+         m_config
+     );
+  private final RamseteAutonomousCommand m_autoCommand = new RamseteAutonomousCommand(m_driveTrain, m_trajectory, m_config);
   private final XboxController m_driveController = new XboxController(0);
 
 
@@ -67,6 +110,7 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
+
     return m_autoCommand;
   }
 }
--- src/main/java/frc4536/robot/subsystems/DriveTrain.java
+++ src/main/java/frc4536/robot/subsystems/DriveTrain.java
@@ -3,12 +3,17 @@ package frc4536.robot.subsystems;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc4536.lib.ISmartMotor;
 
 public class DriveTrain extends SubsystemBase {
     private final ISmartMotor m_leftMotor, m_rightMotor; 
     private final DifferentialDrive m_drive; 
+    private final DifferentialDriveOdometry m_odometry;
     private final AHRS m_navx;
 
     public DriveTrain(ISmartMotor leftMotor, ISmartMotor rightMotor, AHRS navx) {
@@ -17,6 +22,7 @@ public class DriveTrain extends SubsystemBase {
         m_rightMotor = rightMotor;
         m_navx = navx;
         m_drive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+        m_odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(getHeading()));
     }
 
     public void curvatureDrive(double speed, double rotation, boolean quickTurn) {
@@ -36,7 +42,20 @@ public class DriveTrain extends SubsystemBase {
         m_rightMotor.resetEncoder();
     }
 
+    public void setVoltage(double left, double right) {
+        m_leftMotor.setVolt(left);
+        m_rightMotor.setVolt(right);
+    }
+
     public double getDistance() {
         return (m_leftMotor.getDistance() + m_rightMotor.getDistance())/2;
+    }
+
+    public Pose2d getPose() {
+        return m_odometry.getPoseMeters();
+    }
+
+    public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+        return new DifferentialDriveWheelSpeeds(m_rightMotor.getSpeed(), m_leftMotor.getSpeed());
     }
 }
