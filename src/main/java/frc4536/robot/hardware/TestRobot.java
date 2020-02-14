cd6d65ff88a7a72e61d78fe02a90d1c33f76a031
crn-caro <crn@7fff.com>
2020-02-14 15:21:43 -0600

    swapped drivetrain and shooter motors

--- src/main/java/frc4536/robot/hardware/TestRobot.java
+++ src/main/java/frc4536/robot/hardware/TestRobot.java
@@ -28,8 +28,8 @@ public class TestRobot implements RobotFrame {
                                                            kRamseteZeta,
                                                            kWheelDiameterInches);
 
-    IEncoderMotor m_topFlywheel = new VirtualEncoderMotor("Top Flywheel",8.0*0.478779);
-    IEncoderMotor m_bottomFlywheel = new VirtualEncoderMotor("Bottom Flywheel",8.0*0.478779);
+    IEncoderMotor m_topFlywheel = new PWMEncoderMotor(new SpeedControllerGroup(new Spark(0), new Spark(1)), m_leftEncoder, 2048);
+    IEncoderMotor m_bottomFlywheel = new PWMEncoderMotor(new SpeedControllerGroup(new Spark(2), new Spark(3)), m_rightEncoder, 2048);
     VirtualMotor m_intakeMotor = new VirtualMotor("Intake Motor");
     VirtualMotor m_beltMotor = new VirtualMotor("Belt Motor");
     VirtualMotor m_climberArmMotor = new VirtualMotor("Climber Motor");
@@ -40,8 +40,8 @@ public class TestRobot implements RobotFrame {
     AHRS m_navx = new AHRS();
     Encoder m_leftEncoder = new Encoder(0,1);
     Encoder m_rightEncoder = new Encoder(2,3);
-    PWMEncoderMotor m_leftMotors = new PWMEncoderMotor(new SpeedControllerGroup(new Spark(0), new Spark(1)), m_leftEncoder, 2048);
-    PWMEncoderMotor m_rightMotors = new PWMEncoderMotor(new SpeedControllerGroup(new Spark(2), new Spark(3)), m_rightEncoder, 2048);
+    IEncoderMotor m_leftMotors = new VirtualEncoderMotor("Top Flywheel",8.0*0.478779); 
+    IEncoderMotor m_rightMotors = new VirtualEncoderMotor("Bottom Flywheel",8.0*0.478779); 
 
     @Override
     public RobotConstants getConstants() {
