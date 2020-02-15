6e9f84914096f51d62a245bc2bec58beff3c7a03
Kevin Tran <kinzerfest@gmail.com>
2020-02-14 21:58:27 -0600

    Made the drivetrain motors drivetrain motors again

--- src/main/java/frc4536/robot/hardware/TestRobot.java
+++ src/main/java/frc4536/robot/hardware/TestRobot.java
@@ -39,10 +39,10 @@ public class TestRobot implements RobotFrame {
     AHRS m_navx = new AHRS();
     Encoder m_leftEncoder = new Encoder(0,1);
     Encoder m_rightEncoder = new Encoder(2,3);
-    IEncoderMotor m_topFlywheel = new PWMEncoderMotor(new SpeedControllerGroup(new Spark(0), new Spark(1)), m_leftEncoder, 2048);
-    IEncoderMotor m_bottomFlywheel = new PWMEncoderMotor(new SpeedControllerGroup(new Spark(2), new Spark(3)), m_rightEncoder, 2048);
-    IEncoderMotor m_leftMotors = new VirtualEncoderMotor("Top Flywheel",8.0*0.478779); 
-    IEncoderMotor m_rightMotors = new VirtualEncoderMotor("Bottom Flywheel",8.0*0.478779); 
+    IEncoderMotor m_topFlywheel = new VirtualEncoderMotor("Top Flywheel",8.0*0.478779);
+    IEncoderMotor m_bottomFlywheel = new VirtualEncoderMotor("Bottom Flywheel",8.0*0.478779);
+    IEncoderMotor m_leftMotors = new PWMEncoderMotor(new SpeedControllerGroup(new Spark(0), new Spark(1)), m_leftEncoder, 2048);
+    IEncoderMotor m_rightMotors = new PWMEncoderMotor(new SpeedControllerGroup(new Spark(2), new Spark(3)), m_rightEncoder, 2048);
 
     @Override
     public RobotConstants getConstants() {
