d30cfda4b1517747242f6b5d66f9a92bf0f88960
Sophieingersoll <singersoll001@stpaul.k12.mn.us>
2020-02-20 19:04:10 -0600

    commented out top limit switch

--- src/main/java/frc4536/robot/subsystems/Climber.java
+++ src/main/java/frc4536/robot/subsystems/Climber.java
@@ -7,18 +7,19 @@ import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Climber extends SubsystemBase {
     private SpeedController m_armMotor, m_winchMotor;
-    private DigitalInput m_topLimitSwitch;
+   // private DigitalInput m_topLimitSwitch;
     private DigitalInput m_bottomLimitSwitch;
     private Counter m_bottomLimitSwitchCounter;
     private Counter m_topLimitSwitchCounter;
 
-    public Climber(SpeedController armMotor, SpeedController winchMotor, DigitalInput bottomLimitSwitch, DigitalInput topLimitSwitch) {
+    public Climber(SpeedController armMotor, SpeedController winchMotor, DigitalInput bottomLimitSwitch//, DigitalInput topLimitSwitch
+    ) {
         m_winchMotor = winchMotor;
         m_armMotor = armMotor;
         m_bottomLimitSwitch = bottomLimitSwitch;
-        m_topLimitSwitch = topLimitSwitch;
+        //m_topLimitSwitch = topLimitSwitch;
         m_bottomLimitSwitchCounter = new Counter(m_bottomLimitSwitch);
-        m_topLimitSwitchCounter = new Counter(m_topLimitSwitch);
+       // m_topLimitSwitchCounter = new Counter(m_topLimitSwitch);
         m_armMotor.setInverted(true);
         m_bottomLimitSwitchCounter.reset();
         m_topLimitSwitchCounter.reset();
