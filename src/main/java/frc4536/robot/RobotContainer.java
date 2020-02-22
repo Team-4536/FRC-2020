e9769cfea7d3a274196c3eea334a544f0dbadf34
iCalculated <fufa0001@gmail.com>
2020-02-22 10:56:42 -0600

    added trajectory (no commands)

--- src/main/java/frc4536/robot/RobotContainer.java
+++ src/main/java/frc4536/robot/RobotContainer.java
@@ -21,6 +21,7 @@ import frc4536.robot.commands.*;
 import frc4536.robot.hardware.*;
 import frc4536.robot.subsystems.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -155,46 +156,20 @@ public class RobotContainer {
 
 
     public void generateAutoCommands() {
-        Pose2d startPosition = new Pose2d(m_xInitial.getDouble(0.0), m_yInitial.getDouble(0.0), Rotation2d.fromDegrees(0));
-        Pose2d shootingPosition = new Pose2d(7.3, -0.465, Rotation2d.fromDegrees(12));
-        Pose2d endTrench = new Pose2d(7.341, -0.465, Rotation2d.fromDegrees(0));
-        Pose2d twoBallPosition = new Pose2d(6.312, -2.903, Rotation2d.fromDegrees(-90));
-        Pose2d testPosition = new Pose2d(4,-3, Rotation2d.fromDegrees(0));
-        // trajectory from shooting position to end of trench
-        Trajectory initToEnd = TrajectoryGenerator.generateTrajectory(
-                startPosition,
-                List.of(new Translation2d(5.106, -0.465)),
-                endTrench,
-                m_driveTrain.getConfig().setReversed(false)
-        );
-        Trajectory shotToEnd = TrajectoryGenerator.generateTrajectory(
-                shootingPosition,
-                List.of(new Translation2d(5.106, -0.465)),
-                endTrench,
-                m_driveTrain.getConfig().setReversed(false)
-        );
-        // return trajectory
-        Trajectory endToShoot = TrajectoryGenerator.generateTrajectory(
-                endTrench,
-                List.of(new Translation2d(5.407, -1.758)),
-                shootingPosition,
-                m_driveTrain.getConfig().setReversed(true)
-        );
+        
+        Pose2d startPosition = new Pose2d(3.1,-0.75,new Rotation2d(0));
+        Pose2d shootPosition = new Pose2d(5.0,-0.75,new Rotation2d(4.8, 1.7)); //hypothetically, use angle
+        Pose2d trenchEndPosition = new Pose2d(10,-1.0,new Rotation2d(1.4,-0.7));
 
-        Trajectory shootTo2Ball = TrajectoryGenerator.generateTrajectory(
-                shootingPosition,
-                List.of(new Translation2d(5.106, -1.7)),
-                twoBallPosition,
-                m_driveTrain.getConfig().setReversed(false)
-        );
-
-        Trajectory shootAgain = TrajectoryGenerator.generateTrajectory(
-                twoBallPosition,
-                List.of(new Translation2d(5.106, -1.7)),
-                shootingPosition,
-                m_driveTrain.getConfig().setReversed(true)
-        );
-
+        Trajectory startToShoot = TrajectoryGenerator.generateTrajectory(startPosition, new ArrayList<Translation2d>(), shootPosition, m_driveTrain.getConfig().setReversed(false));
+        Trajectory shootToEnd = TrajectoryGenerator.generateTrajectory(shootPosition, new ArrayList<Translation2d>(), trenchEndPosition, m_driveTrain.getConfig().setReversed(false));
+        Trajectory endToShoot = TrajectoryGenerator.generateTrajectory(trenchEndPosition, new ArrayList<Translation2d>(), shootPosition, m_driveTrain.getConfig().setReversed(true));
+        final Command m_trenchAuto = new SequentialCommandGroup(
+                m_driveTrain.scurveTo(startToShoot),
+                m_driveTrain.scurveTo(shootToEnd),
+                m_driveTrain.scurveTo(endToShoot)
+        )
+        /*
         final Command m_trenchAuto = new SequentialCommandGroup(
                 m_driveTrain.scurveTo(initToEnd).raceWith(new IntakeCommands(m_intake, m_conveyor)), //scurve to balls with the intake out
                 m_driveTrain.scurveTo(endToShoot).raceWith(m_shooter.spinUp(() -> Constants.SHOOTER_RPS_TOP, () -> Constants.SHOOTER_RPS_BOTTOM)), //scurve to the shooting position and preemptively spin up the shooter
@@ -216,16 +191,17 @@ public class RobotContainer {
                 new ShootCommand(m_shooter, m_conveyor)
 
         );
-
+        
         final Command m_testAuto = new SequentialCommandGroup( m_driveTrain.scurveTo(
                 TrajectoryGenerator.generateTrajectory(
                         startPosition, 
                         List.of(new Translation2d(
                                 m_xInitial.getDouble(0.0)+3,0)), testPosition, m_driveTrain.getConfig().setReversed(true))));
+        */
 
         m_chooser.setDefaultOption("Trench Auto", m_trenchAuto);
-        m_chooser.addOption("Eight Ball Auto", m_eightBallAuto);
-        m_chooser.addOption("Test Auto", m_testAuto);
+        //m_chooser.addOption("Eight Ball Auto", m_eightBallAuto);
+        //m_chooser.addOption("Test Auto", m_testAuto);
     }
 
     /**
