b0bfc5753e01997baca3d9b9ba702a13a6990037
iCalculated <fufa@gmail.com>
2020-02-23 14:59:35 -0600

    overloaded shoot command

--- src/main/java/frc4536/robot/commands/ShootCommand.java
+++ src/main/java/frc4536/robot/commands/ShootCommand.java
@@ -1,45 +1,40 @@
 package frc4536.robot.commands;
 
 import java.util.function.DoubleSupplier;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+
+import edu.wpi.first.wpilibj2.command.*;
 import frc4536.robot.Constants;
 import frc4536.robot.subsystems.Conveyor;
 import frc4536.robot.subsystems.Shooter;
 
 public class ShootCommand extends ParallelCommandGroup {
-    public ShootCommand(Shooter shooter, Conveyor conveyor, DoubleSupplier topRPS, DoubleSupplier bottomRPS) {
+    public ShootCommand(Shooter shooter, Conveyor conveyor, DoubleSupplier topRPS, DoubleSupplier bottomRPS, double shootTime) {
+      Command condition;
+      if(shootTime == -1) {
+        condition = new WaitUntilCommand(shooter::ready);
+      }
+      else {
+        condition = new WaitCommand(shootTime);
+      }
       addRequirements(conveyor);
       addCommands(
         shooter.spinUp(topRPS, bottomRPS),
-        new WaitUntilCommand(shooter::ready).andThen(new ParallelCommandGroup(
+        condition.andThen(new ParallelCommandGroup(
           new RunCommand(conveyor::lowerTop),
           new RunCommand(() -> conveyor.moveConveyor(Constants.CONVEYOR_SHOOT_SPEED))
         ))
       );
     }
 
-    public ShootCommand(Shooter shooter, Conveyor conveyor) {
-      addRequirements(conveyor);
-      addCommands(
-        shooter.spinUp(() -> Constants.SHOOTER_RPS_TOP, () -> Constants.SHOOTER_RPS_BOTTOM),
-        new WaitUntilCommand(shooter::ready).andThen(new ParallelCommandGroup(
-          new RunCommand(conveyor::lowerTop),
-          new RunCommand(() -> conveyor.moveConveyor(Constants.CONVEYOR_SHOOT_SPEED))
-        ))
-      );
+  public ShootCommand(Shooter shooter, Conveyor conveyor, DoubleSupplier topRPS, DoubleSupplier bottomRPS) {
+    this(shooter, conveyor, topRPS, bottomRPS, -1);
+  }
+
+  public ShootCommand(Shooter shooter, Conveyor conveyor) {
+      this(shooter, conveyor, () -> Constants.SHOOTER_RPS_TOP, () -> Constants.SHOOTER_RPS_BOTTOM, -1);
     }
 
     public ShootCommand(Shooter shooter, Conveyor conveyor, double shootTime) {
-      addRequirements(conveyor);
-      addCommands(
-        shooter.spinUp(() -> Constants.SHOOTER_RPS_TOP, () -> Constants.SHOOTER_RPS_BOTTOM),
-        new WaitCommand(shootTime).andThen(new ParallelCommandGroup(
-          new RunCommand(conveyor::lowerTop),
-          new RunCommand(() -> conveyor.moveConveyor(Constants.CONVEYOR_SHOOT_SPEED))
-        ))
-      );
+      this(shooter, conveyor, () -> Constants.SHOOTER_RPS_TOP, () -> Constants.SHOOTER_RPS_BOTTOM, shootTime);
     }
 }
