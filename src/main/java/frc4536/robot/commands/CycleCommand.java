943ddb90f106604b7b55aa089c098e255ae2173a
iCalculated <fufa0001@gmail.com>
2020-02-23 16:00:38 -0600

    added cycling command

---
+++ src/main/java/frc4536/robot/commands/CycleCommand.java
@@ -0,0 +1,31 @@
+package frc4536.robot.commands;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc4536.robot.Constants;
+import frc4536.robot.subsystems.Conveyor;
+import frc4536.robot.subsystems.DriveTrain;
+import frc4536.robot.subsystems.Intake;
+import frc4536.robot.subsystems.Shooter;
+
+import java.util.ArrayList;
+
+public class CycleCommand extends SequentialCommandGroup {
+  CycleCommand(DriveTrain driveTrain, Shooter shooter, Conveyor conveyor) {
+    Pose2d shootPosition = new Pose2d(6.0, -0.75, new Rotation2d(4.8, 0.8));
+    Pose2d loadingStationPosition = new Pose2d(10.0, -0.75, new Rotation2d(4.8, -0.8)); // I have *no* idea where this is, check in weaver
+    Trajectory toShoot = TrajectoryGenerator.generateTrajectory(driveTrain.getPose(), new ArrayList<Translation2d>(), shootPosition, driveTrain.getConfig().setReversed(true));
+    Trajectory toLoad = TrajectoryGenerator.generateTrajectory(shootPosition, new ArrayList<Translation2d>(), loadingStationPosition, driveTrain.getConfig().setReversed(false));
+    addRequirements(driveTrain, shooter, conveyor);
+    addCommands(
+      driveTrain.scurveTo(toShoot),
+      new VisionToTargetCommand(driveTrain).raceWith(shooter.spinUp(() -> Constants.SHOOTER_RPS_TOP, () -> Constants.SHOOTER_RPS_BOTTOM)),
+      new ShootCommand(shooter, conveyor, 0.0).withTimeout(3),
+      driveTrain.scurveTo(toLoad)
+      );
+  } // TODO: race with any controller input to cancel
+}
