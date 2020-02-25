fc2098f95b600bbb2270ea328cb6035e7412833f
MisterMartin001 <32553884+MisterMartin001@users.noreply.github.com>
2020-02-25 16:24:40 -0600

    manual conveyor

--- src/main/java/frc4536/robot/RobotContainer.java
+++ src/main/java/frc4536/robot/RobotContainer.java
@@ -92,9 +92,18 @@ public class RobotContainer {
         new JoystickButton(m_driveController, Button.kBumperRight.value)
                 .whileHeld(new IntakeCommands(m_intake, m_conveyor));   //Intake
         new JoystickButton(m_driveController, Button.kB.value)          //Spin up conveyor
-                .whileHeld(() -> m_conveyor.moveConveyor(Constants.CONVEYOR_SHOOT_SPEED), m_conveyor);
+                .whileHeld(
+                        () -> {m_conveyor.moveConveyor(Constants.CONVEYOR_SHOOT_SPEED);
+                                m_conveyor.lowerTop();
+                }, m_conveyor);
         new JoystickButton(m_driveController, Button.kY.value)          //Spin up shooter
                 .whileHeld(new ShootCommand(m_shooter, m_conveyor, () -> top.getDouble(Constants.SHOOTER_RPS_TOP), () -> bot.getDouble(Constants.SHOOTER_RPS_BOTTOM)));
+        /*new JoystickButton(m_driveController, Button.kX.value)          //Example pose reset
+                .whileHeld(() -> m_driveTrain.resetPose(Poses.HARD_RESET));
+        new JoystickButton(m_driveController, Button.kX.value)        //Example pose reset
+                .whileHeld(() -> m_driveTrain.resetPose(loadingStationPose));*/
+        new JoystickButton(m_driveController, Button.kX.value)          //reverse move conveyor 
+                .whileHeld(new RunCommand(() -> m_conveyor.moveConveyor(-0.5), m_conveyor));
 
         //Operator Controller
         new JoystickButton(m_operatorJoystick, 2)   //Spinup shooter
